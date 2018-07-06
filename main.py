3c36f7482f5f73647012d212ca828d77b3a822fe
KacperFKorban <kacper.f.korban@gmail.com>
2018-07-06 11:46:50 +0200

    settings

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-def launchGame:
-    
+import pygame
+from pygame.locals import *
+import settings
+
+def launchGame():
+    pygame.init()
+    st = Settings()
+    screen = pygame.display.set_mode((st.screenWidth, st.screenHeight))
+    pygame.display.set_caption('2DGame')
+    screen.fill((255,255,255))
+    pygame.display.flip()
+    runningFlag = True
+    while running:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          runningFlag = False
---
+++ settings.py
@@ -0,0 +1,4 @@
+class Settings:
+    def __init__(self):
+        self.screenWidth = 1200
+        self.screenHight = 600
